20636641d3419199a247e1cad2ee21ec959725a7
Igor <ig.zuban1ch@gmail.com>
2023-07-28 21:30:08 +0300

    feat: add user details

---
+++ sb-chat-app/src/main/java/com/example/wschatapp/security/DefaultUserDetails.java
@@ -0,0 +1,60 @@
+package com.example.wschatapp.security;
+
+import com.example.wschatapp.entity.UserRoles;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+public class DefaultUserDetails implements UserDetails {
+
+    private final String id;
+    private final String login;
+    private final String password;
+    private final List<UserRoles> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(UserRoles::name)
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
