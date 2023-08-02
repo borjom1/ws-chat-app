4fc77d5b1a146eb750cd7b0d37433a424ad07224
Igor <ig.zuban1ch@gmail.com>
2023-08-02 22:36:21 +0300

    feat: create page for not found resources

---
+++ r-chat-app/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/loading.css';
+
+const NotFound = () => {
+  return (
+    <div className='w-full h-[100vh] flex items-center'>
+      <p className='mx-auto w-max text-silver-bf text-2xl font-medium'>Not found page 404</p>
+    </div>
+  );
+};
+
+export default NotFound;
