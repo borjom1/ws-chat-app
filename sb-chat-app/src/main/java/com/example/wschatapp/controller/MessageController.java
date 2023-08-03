9debac31a1d50760a8c092c5c5eee75d4eee871f
Igor <ig.zuban1ch@gmail.com>
2023-08-03 16:57:13 +0300

    feat(MessageController): persist messages

--- sb-chat-app/src/main/java/com/example/wschatapp/controller/MessageController.java
+++ sb-chat-app/src/main/java/com/example/wschatapp/controller/MessageController.java
@@ -1,7 +1,8 @@
 package com.example.wschatapp.controller;
 
 import com.example.wschatapp.dto.IncomeTextMessage;
-import com.example.wschatapp.dto.OutcomeTextOutcomeMessage;
+import com.example.wschatapp.dto.OutcomeTextMessage;
+import com.example.wschatapp.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -9,22 +10,19 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
-import static com.example.wschatapp.dto.MessageType.MESSAGE;
-import static java.time.ZonedDateTime.now;
-
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class MessageController {
 
     private final SimpMessagingTemplate messagingTemplate;
+    private final ChatService chatService;
 
     @MessageMapping("/send.message")
-    public void handleMessage(@Payload IncomeTextMessage message) {
+    public void handleIncomeMessage(@Payload IncomeTextMessage message) {
         log.info("MSG={}", message);
-
-        var outcomeMsg = new OutcomeTextOutcomeMessage(MESSAGE, message.sender(), now(), message.text());
-        messagingTemplate.convertAndSend("/queue/%s".formatted(message.receiver()), outcomeMsg);
+        OutcomeTextMessage textMessage = chatService.saveMessage(message);
+        messagingTemplate.convertAndSend("/queue/%s".formatted(message.receiverId()), textMessage);
     }
 
 }
