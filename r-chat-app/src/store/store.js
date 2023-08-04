639dd84e0af4385b8c715faf5ed9dd8e0b60e347
Igor <ig.zuban1ch@gmail.com>
2023-08-04 14:49:57 +0300

    feat(store): combine chat-reducer with auth-reducer

--- r-chat-app/src/store/store.js
+++ r-chat-app/src/store/store.js
@@ -1,10 +1,14 @@
-import {configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
-import authReducer from "./slice/authSlice";
+import {combineReducers, configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
+import authReducer, {authSlice} from "./slice/authSlice";
+import chatReducer, {chatSlice} from "./slice/chatSlice";
+
+const rootReducer = combineReducers({
+  [authSlice.name]: authReducer,
+  [chatSlice.name]: chatReducer
+});
 
 export default configureStore({
-  reducer: {
-    auth: authReducer
-  },
+  reducer: rootReducer,
   middleware: getDefaultMiddleware({
       serializableCheck: false
   })
