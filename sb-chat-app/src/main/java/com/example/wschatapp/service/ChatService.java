8a09673b984b632790e3a7ac7da2ac92c7e29329
Igor <ig.zuban1ch@gmail.com>
2023-08-07 20:32:37 +0300

    feat(ChatService): implement method for returning all duo chats that user is related to

--- sb-chat-app/src/main/java/com/example/wschatapp/service/ChatService.java
+++ sb-chat-app/src/main/java/com/example/wschatapp/service/ChatService.java
@@ -1,8 +1,12 @@
 package com.example.wschatapp.service;
 
+import com.example.wschatapp.dto.ChatDto;
 import com.example.wschatapp.dto.IncomeTextMessage;
 import com.example.wschatapp.dto.OutcomeTextMessage;
 
+import java.util.List;
+
 public interface ChatService {
     OutcomeTextMessage saveMessage(IncomeTextMessage incomeMessage);
+    List<ChatDto> getAllDuoChats(String id);
 }
--- sb-chat-app/src/main/java/com/example/wschatapp/service/impl/ChatServiceImpl.java
+++ sb-chat-app/src/main/java/com/example/wschatapp/service/impl/ChatServiceImpl.java
@@ -1,9 +1,11 @@
 package com.example.wschatapp.service.impl;
 
+import com.example.wschatapp.dto.ChatDto;
 import com.example.wschatapp.dto.IncomeTextMessage;
+import com.example.wschatapp.dto.MessageType;
 import com.example.wschatapp.dto.OutcomeTextMessage;
 import com.example.wschatapp.entity.ChatEntity;
-import com.example.wschatapp.entity.ChatType;
+import com.example.wschatapp.entity.MessageEntity;
 import com.example.wschatapp.entity.UserEntity;
 import com.example.wschatapp.repository.ChatRepository;
 import com.example.wschatapp.service.ChatService;
@@ -12,9 +14,14 @@ import com.example.wschatapp.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
+
+import static com.example.wschatapp.entity.ChatType.DUO;
+import static com.example.wschatapp.entity.ChatType.GROUP;
 
 @Slf4j
 @Service
@@ -26,6 +33,7 @@ public class ChatServiceImpl implements ChatService {
     private final ChatRepository chatRepository;
 
     @Override
+    @Transactional
     public OutcomeTextMessage saveMessage(IncomeTextMessage incomeMessage) {
         log.info("-> saveMessage(): {}", incomeMessage);
 
@@ -39,10 +47,15 @@ public class ChatServiceImpl implements ChatService {
 
         duoChat = optDuoChat.orElseGet(() -> {
             ChatEntity newChat = ChatEntity.builder()
-                    .members(List.of(sender, receiver))
-                    .type(ChatType.DUO)
+                    .type(DUO)
                     .membersLimit(2L)
                     .build();
+
+            // add users to recently created chat
+            newChat.addUser(sender);
+            newChat.addUser(receiver);
+            userService.save(sender, receiver);
+
             return chatRepository.save(newChat);
         });
 
@@ -52,4 +65,44 @@ public class ChatServiceImpl implements ChatService {
         return persistedMessage;
     }
 
+    @Override
+    public List<ChatDto> getAllDuoChats(String userId) {
+        log.info("-> getAllDuoChats()");
+
+        UserEntity user = userService.find(userId);
+        return user.getChats().stream()
+                .filter(chat -> chat.getType().equals(DUO))
+                .map(chat -> {
+
+                    // in this case we need to return duo-chat member regardless of that who sent message
+                    MessageEntity lastMessage = messageService.getLastMessage(chat).orElseThrow();
+                    UserEntity member = getDuoChatMember(chat, userId);
+
+                    return new ChatDto(
+                            member.getName(),
+                            new OutcomeTextMessage(
+                                    MessageType.MESSAGE,
+                                    member.getId(),
+                                    member.getLogin(),
+                                    lastMessage.getCreated().atZone(ZoneId.systemDefault()),
+                                    lastMessage.getText()
+                            )
+                    );
+                })
+                .sorted((chat1, chat2) ->
+                        chat2.lastMessage().getTimestamp()
+                        .compareTo(chat1.lastMessage().getTimestamp()))
+                .toList();
+    }
+
+    private UserEntity getDuoChatMember(ChatEntity chat, String userId) {
+
+        if (chat.getType().equals(GROUP)) {
+            throw new IllegalArgumentException("accepted chat is not duo");
+        }
+
+        List<UserEntity> members = chat.getMembers();
+        return members.get(0).getId().equals(userId) ?
+                members.get(1) : members.get(0);
+    }
 }
