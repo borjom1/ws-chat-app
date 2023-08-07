546a17d250bb37d09fecf7953ee9bf8566b6544d
Igor <ig.zuban1ch@gmail.com>
2023-08-07 20:42:54 +0300

    feat(chatSlice): create async thunk and extra-reducers for retrieving duo chats from api

--- r-chat-app/src/store/slice/chatSlice.js
+++ r-chat-app/src/store/slice/chatSlice.js
@@ -1,19 +1,45 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import axios from "axios";
+import env from 'react-dotenv';
+import {getUser, setUser} from "../../utils/localStorage";
+import {getTime} from "../../utils/dateParser";
+import {useNavigate} from "react-router-dom";
+
+export const getDuoChats = createAsyncThunk(
+  'chat/getDuoChats',
+  async (_, {rejectWithValue}) => {
+    try {
+      const response = await axios.get(`${env.API_URL}/user/chats`, {
+        headers: {
+          Authorization: `Bearer ${getUser().access}`
+        }
+      });
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
 
 export const chatSlice = createSlice({
   name: 'chat',
   initialState: {
     stompClient: null,
     chats: [],
-    selectedChat: null
+    selectedChat: null,
+    isRequestRejected: false
   },
   reducers: {
+    setChats: (state, {payload: {targetChats}}) => {
+      state.chats = targetChats;
+    },
     updateChat: ({chats}, {payload: {senderId, login, newMessage}}) => {
 
       const foundChat = chats.find(chat => chat.userId === senderId);
 
       if (!foundChat) {
         chats.push({
+          username: 'Unknown',
           userId: senderId,
           login,
           messages: [newMessage]
@@ -34,6 +60,35 @@ export const chatSlice = createSlice({
     },
     setStompClient: (state, {payload: newStompClient}) => {
       state.stompClient = newStompClient;
+    }
+  },
+  extraReducers: {
+    [getDuoChats.rejected]: (state) => {
+      console.log('getDuoChats.rejected')
+      state.isRequestRejected = true;
+      state.chats = [];
+      setUser(null);
+
+      const navigate = useNavigate();
+      navigate('/sign_in');
+    },
+    [getDuoChats.fulfilled]: (state, {payload: retrievedChats}) => {
+      state.isRequestRejected = false;
+      state.chats = retrievedChats.map(chat => {
+        const {lastMessage: {timestamp, senderId, senderLogin}} = chat;
+
+        return {
+          username: chat.name,
+          userId: senderId,
+          login: senderLogin,
+          messages: [{
+            id: timestamp,
+            text: chat.lastMessage.text,
+            time: getTime(new Date(timestamp)),
+            isOwn: senderId === getUser().id
+          }]
+        };
+      });
     }
   }
 });
