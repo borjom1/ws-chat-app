d446f00ac17482e1c3e1abb2db56469d9cf05d6e
Igor <ig.zuban1ch@gmail.com>
2023-08-07 20:46:34 +0300

    feat: add avatar and last online properties

--- r-chat-app/src/components/Chat.jsx
+++ r-chat-app/src/components/Chat.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
-import { BsFillPersonFill } from 'react-icons/bs';
-import { AiOutlineSend } from 'react-icons/ai';
+import avatar from '../assets/user.png';
 import Message from './Message';
 import MessageField from './MessageField';
 import Button from './Button';
 import '../styles/scrollbar.css';
 import classNames from 'classnames';
 import { SlLayers } from 'react-icons/sl';
+import { AiOutlineFieldTime } from 'react-icons/ai';
+import {BiMailSend} from "react-icons/bi";
 
 const Chat = ({ className, meta, onSendClick }) => {
 
@@ -33,21 +34,26 @@ const Chat = ({ className, meta, onSendClick }) => {
             <SlLayers size={28} color={'#4E4E4E'}/>
             <p className='text-[#4E4E4E]'>Select any chat to start communication...</p>
         </div> :
-        <>
-          <div className="h-[12%] bg-[#282828] rounded-t-xl py-2.5 px-8 flex gap-3 items-center">
-            <div className='bg-[#1e1e1e] rounded-full py-2 px-2'>
-              <BsFillPersonFill size={30} color='#E8E8E8' />
+        <div className='h-full flex flex-col gap-4 cursor-default'>
+          <div className="h-[12%] bg-[#282828] rounded-t-xl py-2.5 px-6 flex gap-3 items-center">
+            <img src={meta.avatar ?? avatar} width={43} height={43} alt={'avatar'}/>
+            <div className='w-full flex flex-col gap-1.5'>
+              <h1 className="text-[#EFEFEF] text-xl leading-none">{meta.username}</h1>
+              <h2 className="text-[#969696] text-xs leading-none">@{meta.login}</h2>
             </div>
-            <h1 className="text-platinum-e8 text-2xl font-medium">{meta.login}</h1>
+            <div className='flex gap-1 items-center'>
+              <AiOutlineFieldTime size={20} color={'#636363'}/>
+              <p className='w-max font-medium text-xs text-[#636363] leading-none cursor-default'>Last seen at 15:49</p>
+            </div>
           </div>
 
-          <div className='h-[70%] border-2 border-[#282828] flex-col flex gap-4 mt-3 mx-2 rounded-md py-3 px-3 overflow-y-scroll'>
+          <div className='h-[75%] border-2 border-[#282828] bg-[#212121] flex-col flex gap-4 mx-6 rounded-md py-3 px-3 overflow-y-scroll'>
             {mappedMessages}
           </div>
 
-          <div className='h-[18%] mx-2 mt-3 flex gap-2 justify-between'>
+          <div className='h-[13%] px-6 flex gap-2 justify-between'>
             <MessageField
-              className='w-[90%] h-[48%]'
+              className='w-[90%] h-[41px]'
               text={msgText}
               onChange={value => setMsgText(value)}
             />
@@ -57,11 +63,11 @@ const Chat = ({ className, meta, onSendClick }) => {
                 onSendClick(msgText);
               }}
               bgClassName={'px-2 py-2 hover:bg-[#2B4071]'}
-              icon={<AiOutlineSend size={25} color='#DBDBDB' />}
+              icon={<BiMailSend size={25} color='#DBDBDB' />}
               bgColor={'blue-39'}
             />
           </div>
-        </>
+        </div>
       }
     </div>
   );
